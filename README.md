359a6c1c71a9122bcdb322bcd4e3cdb8183080bd
xLE0N0x <ldo.aguilar@gmail.com>
2025-03-15 23:31:06 -0300

    Correcciones al codigo, adicion de README.md

---
+++ README.md
@@ -0,0 +1,30 @@
+# Sorteo de Amigos Secretos
+
+Este proyecto permite gestionar una lista de amigos y realizar un sorteo para seleccionar un amigo secreto de forma aleatoria.
+
+## Características
+
+- Agregar amigos a una lista evitando duplicados.
+- Realizar un sorteo aleatorio entre los amigos agregados.
+- Mostrar el resultado del sorteo.
+- Reiniciar la lista y el sorteo en cualquier momento.
+
+## Tecnologías Utilizadas
+
+- JavaScript
+- HTML
+- CSS
+
+## Instrucciones de Uso
+
+1. **Agregar amigos**: Escriba el nombre de un amigo en el campo de entrada y presione `Enter` o ultilice el botono `Añadir`.
+2. **Realizar el sorteo**: Haga clic en el botón de sorteo para seleccionar un amigo secreto de manera aleatoria. Podrá continuar sorteando amigos hasta que todos los nombres agregados al comienzo hayan sido sorteados.
+3. **Reiniciar**: Haga clic en el botón de reinicio para borrar la lista y comenzar de nuevo.
+
+## Contribución
+
+Si deseas mejorar el código o añadir nuevas funcionalidades, siéntete libre de hacer un fork y enviar un pull request.
+
+## Licencia
+
+Este proyecto está bajo la licencia MIT.
--- app.js
+++ app.js
@@ -22,25 +22,22 @@ function agregarAmigo() {
         alert('Por favor, inserte un nombre.');
     } else {
         if (amigos.includes(nombreAmigo.value)) { // Comprueba que 'nombreAmigo' NO se encuentre en array 'amigos' evitando nombres duplicados
-            alert('Este nombre ya se encuentra en la lista, por favor ingrese un nombre nuevo');
+            alert('Este nombre ya se encuentra en la lista, por favor ingrese un nombre distinto');
         } else {
             agregaAmigoLista(nombreAmigo.value, amigos); // Agrega 'nombreAmigo' a arreglo 'amigos'
             modificaElementoID(nombreAmigo, ''); // Limpia Imput 'amigo'
             creaListaAmigos(listaAmigos, amigos); // Crea un listados con todos los nombres contenidos en el arra 'amigos'
             amigo.focus();
-            document.getElementById('btnReinicio').removeAttribute('disabled');
+            cambiaEstadoBoton('btnSorteo', 'encender');
+            cambiaEstadoBoton('btnReinicio', 'encender');
         }
     }
 }
 
 function sortearAmigo() {
-    if (amigos.length < 2) { // Comprueba que array 'amigos' tenga almenos dos nombres para sortear
-        alert('Para sortear al amigo secreto se necesitan al menos dos nombres.');
-    } else {
-        modificaElementoID(listaAmigos, ''); // Limpia lista 'listaAmigos'
-        modificaElementoID(resultado, `Tu amigo secreto sorteado es: ${generaSorteo(amigos)}`); // Publica nombre sorteado en 'resultado'
-
-    }
+    cambiaEstadoBoton('btnAdd', 'apagar');
+    modificaElementoID(listaAmigos, ''); // Limpia lista 'listaAmigos'
+    modificaElementoID(resultado, `Tu amigo secreto sorteado es: ${generaSorteo(amigos)}`); // Publica nombre sorteado en 'resultado'
 }
 
 function reiniciar() { // Función limpia: input 'amigo', lista 'listaAmigos', lista 'resultado', lista 'listaSorteados'
@@ -48,15 +45,20 @@ function reiniciar() { // Función limpia: input 'amigo', lista 'listaAmigos', l
     modificaElementoID(listaAmigos, '');
     modificaElementoID(resultado, '');
     modificaElementoID(listaSorteados, '');
-    amigo.focus(); // Hace focus sobre el input 'amigo' para ingresar nombres
-    amigos.splice(0, amigos.length); // Limpia array 'amigos'
-    amigosSorteados.splice(0, amigosSorteados.length); // Limpia array 'amigosSorteados'
-    document.getElementById('btnSorteo').removeAttribute('disabled'); // Activa boton 'btnSorteo'
-    document.querySelector('#btnReinicio').setAttribute('disabled', 'true'); // Desactiva boton 'btnReinicio'
+    amigo.focus();
+    limpiarArreglo(amigos);
+    limpiarArreglo(amigosSorteados);
+    cambiaEstadoBoton('btnAdd', 'encender')
+    cambiaEstadoBoton('btnSorteo', 'apagar');
+    cambiaEstadoBoton('btnReinicio', 'apagar');
 }
 
 function agregaAmigoLista(nombre, array) { // Funcion que agrega nombre indicado a array indicado
     array.push(nombre);
+}
+
+function limpiarArreglo(arreglo) {
+    arreglo.splice(0, arreglo.length);
 }
 
 function modificaElementoID(elementoID, texto) {
@@ -65,8 +67,15 @@ function modificaElementoID(elementoID, texto) {
 }
 
 function creaListaAmigos(elementoID, array) { // Función que agrega a la lista seleccionada los nombre de amigos del array indicado
-
     modificaElementoID(elementoID, ''); // Limpia la lista para evitar duplicados
+
+    if (elementoID == listaSorteados) {
+        let titulo = document.createElement('h2');
+
+        listaSorteados.appendChild(titulo);
+        titulo.textContent = "Amigos sorteados";
+    }
+
     for (let i = 0; i < array.length; i++) {
         let li = document.createElement('li');
         li.textContent = array[i];
@@ -75,7 +84,6 @@ function creaListaAmigos(elementoID, array) { // Función que agrega a la lista 
 }
 
 function generaSorteo(array) {
-
     let nombreSorteado = Math.floor(Math.random() * array.length); // Generea número aleatorio * por la cantidad total de nombres de amigos
 
     if (amigosSorteados.includes(array[nombreSorteado])) // Condición: si 'nombreSorteado' ya se encuentra en la lista 'amigosSorteados'
@@ -84,9 +92,22 @@ function generaSorteo(array) {
         agregaAmigoLista(array[nombreSorteado], amigosSorteados); // Agrega 'nombreSorteado' a arreglo 'amigosSorteados'
         creaListaAmigos(listaSorteados, amigosSorteados); // Agrega a la lista 'listaSorteados' nombres del array 'amigosSorteados'
         if (amigosSorteados.length === array.length) { // Comprueba que la cantidad de nombre en ambos arrays es igual
-            document.querySelector('#btnSorteo').setAttribute('disabled', 'true'); // Desactiva boton
+            cambiaEstadoBoton('btnSorteo', 'apagar')
         }
         return array[nombreSorteado];
+    }
+}
+
+function cambiaEstadoBoton(botonID, estado) {
+    let estadoBoton = estado;
+
+    switch (estadoBoton) {
+        case 'encender':
+            document.getElementById(botonID).removeAttribute('disabled');
+            break;
+        case 'apagar':
+            document.getElementById(botonID).setAttribute('disabled', true);
+            break;
     }
 }
 
